d5c99f886e8e2f8734e7b071f8ead59de4aa045f
Divine-AC <divineabbachimdirim1@gmail.com>
2026-09-23 10:57:44 +0100

    fix: clean entry point files and vercel rewrite configuration

--- FRONTEND/main.jsx
+++ FRONTEND/main.jsx
@@ -1,19 +1,15 @@
-<script type="module" src="/src/main.jsx"></script>
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { AuthProvider } from './context/AuthContext.jsx'
 import './index.css'
-import { Toaster } from 'react-hot-toast';
-
-// Place <Toaster position="top-right" /> inside your root return component
-
+import { Toaster } from 'react-hot-toast'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <App />
+      <Toaster position="top-right" />
     </AuthProvider>
-    <Toaster position="top-right" />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
